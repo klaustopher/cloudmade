0d27d0ca923afaaf79742ef0820604208af26957
ishubovych@cloudmade.com <ishubovych@cloudmade.com@567a97cc-860d-43c2-b793-ca20abbab3cc>
2009-01-30 16:42:04 +0000

    license headers

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,3 +1,19 @@
+#
+# Copyright 2009 CloudMade.
+#
+# Licensed under the GNU Lesser General Public License, Version 3.0;
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.gnu.org/licenses/lgpl-3.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #Raised when API key is not specified in Connection constructor
 class APIKeyMissingError < Exception
 end
